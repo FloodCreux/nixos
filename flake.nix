d19ba3038aa46a772b497eb6b3889f8558337f8e
Mike Flood <mflood@r1rcm.com>
2024-12-17 20:18:32 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-schema.url = "github:DeterminateSystems/flake-schemas";
+
+    nix-schema = {
+      url = "github:DeterminateSystems/nix-src/flake-schemas";
+      inputs.flake-schemas.follows = "flake-schemas";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-flake = {
+      url = "github:FloodCreux/neovim-ide";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-schemas.follows = "flake-schemas";
+    };
+
+    # Hyprland
+    hypr-binds-flake = {
+      url = "github:hyprland-community/hpr-binds";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Nix Search
+    nix-search = {
+      url = "github:diamondburned/nix-search";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      overlays = import ./lib/overlays.nix { inherit inputs system; };
+
+      pkgs = import inputs.nixpkgs {
+        inherit overlays system;
+        config.allowUnfree = true;
+      };
+
+      homeConfigurations = pkgs.mkHomeConfigurations { };
+      nixosConfigurations = pkgs.mkNixosConfigurations { };
+
+      neovim = homeConfigurations.hprland-hdmi.config.programs.neovim-ide.finalPackage;
+    in
+    {
+      inherit homeConfigurations nixosConfigurations;
+
+      out = { inherit pkgs overlays; };
+
+      schemas =
+        inputs.flake-schemas.schemas
+        // import ./lib/schemas.nix { inherit (inputs) flake-schemas; };
+
+      apps.${system}."nix" = {
+        type = "app";
+        program = "${pkgs.nix-schema}/bin/schema";
+      };
+
+      packages.${system} = {
+        inherit neovim;
+      };
+    };
+}
---
+++ lib/overlays.nix
@@ -0,0 +1,104 @@
+{ inputs, system }:
+
+with inputs;
+
+let
+  nixSearchOverlay = f: p: {
+    nix-search = nix-search.packages.${system}.default;
+  };
+
+  libVersionOverlay = import "${inputs.nixpkgs}/lib/flake-version-info.nix" inputs.nixpkgs;
+
+  libOverlay = f: p: rec {
+    libx = import ./. { inherit (p) lib; };
+    lib =
+      (p.lib.extend (
+        _: _: {
+          inherit (libx) exe removeNewLine;
+        }
+      )).extend
+        libVersionOverlay;
+  };
+
+  buildersOverlay = f: p: {
+    mkHomeConfigurations =
+      {
+        pkgs ? f,
+        extraPkgs ? [ ],
+      }:
+      import ../outputs/hm.nix {
+        inherit
+          extraPkgs
+          inputs
+          pkgs
+          system
+          ;
+      };
+
+    mkNixosConfigurations =
+      {
+        pkgs ? f,
+        extraSystemConfig ? { },
+      }:
+      import ../outputs/os.nix {
+        inherit
+          extraSystemConfig
+          inputs
+          pkgs
+          system
+          ;
+      };
+  };
+
+  treesitterGrammarsOverlay = f: p: {
+    treesitterGrammars = _.withPlugins (p: [
+      p.tree-sitter-c
+      p.tree-sitter-nix
+      p.tree-sitter-haskell
+      p.tree-sitter-python
+      p.tree-sitter-rust
+      p.tree-sitter-markdown
+      p.tree-sitter-markdown-inline
+      p.tree-sitter-comment
+      p.tree-sitter-toml
+      p.tree-sitter-make
+      p.tree-sitter-json
+    ]);
+  };
+
+  xargsOverlay = f: p: {
+    xargs =
+      { hidpi }:
+      {
+        inherit hidpi;
+        inherit (inputs) gh-md-toc penguin-fox;
+        inherit (inputs.rycee-nurpkgs.lib.${system}) buildFirefoxXpiAddon;
+        addons = f.nur.repos.rycee.firefox-addons;
+      };
+  };
+
+  schemaOverlay = f: p: {
+    nix-schema = inputs.nix-schema.packages.${system}.nix.overrideAttrs (old: {
+      doCheck = false;
+      doInstallCheck = false;
+      postInstall =
+        old.postInstall
+        + ''
+          rm $out/bin/nix-*
+          mv $out/bin/nix $out/bin/nix-schema
+        '';
+    });
+  };
+in
+[
+  libOverlay
+  nixSearchOverlay
+  nurpkgs.overlays.default
+  neovim-flake.overlays.${system}.default
+  statix.overlays.default
+  xargsOverlay
+  (import ../home/overlays/hypr-monitor-attached)
+  buildersOverlay
+  treesitterGrammarsOverlay
+  schemaOverlay
+]
