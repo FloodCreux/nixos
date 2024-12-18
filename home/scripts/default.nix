256c689db3d514264102bc28097a8055b09d5126
Mike Flood <mflood@r1rcm.com>
2024-12-18 12:35:47 -0600

    add some lib items

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -8,12 +8,12 @@ let
     }:
     let
       gsk = pkgs.callPackage ./gen-ssh-key.nix { };
-      szp = pkgs.callPackage ./show-zombie-parents.nix { };
     in
+    # szp = pkgs.callPackage ./show-zombie-parents.nix { };
     {
       home.packages = [
         gsk # generate ssh key and add it to the system
-        szp # show zombie parents
+        # szp # show zombie parents
       ] ++ (pkgs.sxm.scripts or [ ]);
     };
 in
--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,13 @@
 { lib }:
 {
+  mkVimBool = val: if val then 1 else 0;
+
+  writeIf = cond: msg: if cond then msg else "";
+
+  withPlugins = cond: plugins: if cond then plugins else [ ];
+
+  withAttrSet = cond: attrSet: if cond then attrSet else { };
+
   /*
     Return first binary executable name of the given derivation
     Type:
